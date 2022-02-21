75658cd21ecb17afd551fd3e0c279b8b19026ce4
charles <chkftm12@gmail.com>
2022-02-21 21:09:53 +0900

    파일 분리

---
+++ hacker-news-project-ts/src/config.ts
@@ -0,0 +1,4 @@
+export const ajax: XMLHttpRequest = new XMLHttpRequest();
+export const content: HTMLElement | null = document.createElement('div');
+export const NEWS_URL: string = 'https://api.hnpwa.com/v0/news/1.json';
+export const CONTENT_URL: string = 'https://api.hnpwa.com/v0/item/@id.json';
